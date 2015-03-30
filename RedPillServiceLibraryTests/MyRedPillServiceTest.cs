69c644a5f75b74e87586a85e02632d710e1d688e
Abdelwahab Sadek <AbdelwahabSadek@gmail.com>
2015-03-30 08:02:03 +0200

    Adding Some test cases

--- RedPillServiceLibraryTests/MyRedPillServiceTest.cs
+++ RedPillServiceLibraryTests/MyRedPillServiceTest.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using RedPillServiceLibrary;
 using RedPillServiceLibrary.RedPillServiceRef;
 
@@ -15,9 +16,9 @@ namespace RedPillServiceLibraryTests
             Service = new MyRedPillService();
         }
 
-
+        #region "Fibonacci Number Tests"
         [TestMethod]
-        public void FibonacciNumberTest()
+        public void FibonacciNumberIndexBetweenPositiveRangeTest()
         {
             long fact1 = Service.FibonacciNumber(5);
             long fact2 = Service.FibonacciNumber(6);
@@ -27,7 +28,32 @@ namespace RedPillServiceLibraryTests
         }
 
         [TestMethod]
-        public void WhatShapeIsThisTest()
+        public void FibonacciNumberIndexBetweenRangeTest()
+        {
+            long fact1 = Service.FibonacciNumber(-5);
+            long fact2 = Service.FibonacciNumber(-6);
+            long fact3 = Service.FibonacciNumber(-7);
+
+            Assert.IsTrue(fact1 == fact3 + fact2);
+        }
+       
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void FibonacciNumberIndexOutOfRangeTest()
+        {
+            long fact1 = Service.FibonacciNumber(93);
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void FibonacciNumberIndexBelowRangeTest()
+        {
+            long fact1 = Service.FibonacciNumber(-93);
+        }
+        #endregion
+
+        #region "What Shape Tests"
+        [TestMethod]
+        public void WhatShapeIsThisAllCasesTest()
         {
             var equatorial = new[] {3, 3, 3};
             var isosceles = new[] {3, 3, 4};
@@ -38,7 +64,23 @@ namespace RedPillServiceLibraryTests
             Assert.IsTrue(retequatorial == TriangleType.Equilateral && retisosceles == TriangleType.Isosceles &&
                           retscalene == TriangleType.Scalene);
         }
+        [TestMethod]
+        public void WhatShapeIsThisErrorTest()
+        {
+            var Error = new[] { 1,2, 1 };
+            TriangleType error = Service.WhatShapeIsThis(Error[0], Error[1], Error[2]);
+            Assert.IsTrue(error == TriangleType.Error);
+        }
+        [TestMethod]
+        public void WhatShapeIsThisZeroTest()
+        {
+            var zero = new[] { 0, 4, 1 };
+            TriangleType Zero = Service.WhatShapeIsThis(zero[0], zero[1], zero[2]);
+            Assert.IsTrue(Zero == TriangleType.Error);
+        }
+        #endregion
 
+        #region "Reverse Words Tests"
         [TestMethod]
         public void ReverseWordsTest()
         {
@@ -46,5 +88,7 @@ namespace RedPillServiceLibraryTests
             const string toReverse = "asdf qwer";
             if (Service != null) Assert.AreEqual(Service.ReverseWords(toReverse), reversed);
         }
+        #endregion
     }
+
 }
